b2fcae9e627c6983661373c9bbdf62625e724046
E Luckie <eluckie.d@gmail.com>
2023-02-13 18:55:24 -0500

    add getPalette( ) function to pull a randomized color palette
    Co-authored-by: Eliot Gronstal <elgrons@users.noreply.github.com>
    Co-authored-by: Molly Rose Donegan <mollyrdonegan@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,15 +1,39 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import ThreadService from './js/threads.js';
+// import ThreadService from './js/thread-service.js';
 
 // Business Logic
 
-async function getThreads(city) {
-  const response = await ThreadService.getThreads(city);
-  if (response.main) {
-    printElements(response, city);
-  } else {
-    printError(response, city);
+function getPalette() {
+  const url = "http://colormind.io/api/";
+  let data = {
+    model : "default",
+    input : [[44,43,44],[90,83,82],"N","N","N"]
   }
+
+  let request = new XMLHttpRequest();
+
+  request.onreadystatechange = function() {
+    if(request.readyState == 4 && request.status == 200) {
+      let palette = JSON.parse(request.responseText).result;
+      console.log(palette);
+    } else {
+      console.log(`${request.status}`);
+    }
+  }
+
+  request.open("POST", url, true);
+  request.send(JSON.stringify(data));
+
 }
+
+//User Interface Logic
+
+// function printElements() {
+//   getPalette();
+// }
+
+// function printError() {
+
+// }

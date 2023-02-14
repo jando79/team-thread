acc1179298c931a9e9904d00d33feb8132df9905
Eliot Gronstal <eliot.lauren@gmail.com>
2023-02-14 10:22:03 -0800

    update UI interface and HTML w/ hidden div and links. WIP: printElements function.
    Co-authored-by: Molly Rose Donegan <mollyrdonegan@gmail.com>
    Co-authored-by: Sarah Andyshak <sarah.andyshak@gmail.com>
    Co-authored-by: Luckie <eluckie.d@gmail.com>
    Co-authored-by: David Jandron  <jandron1211@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,31 +1,88 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import ThreadService from './js/thread-service.js';
+// import GetPalette from './js/getpalette.js';
 
-
-
+// Business Logic
+  
+function getPalette(color1, color2, color3) {
+  const url = "http://colormind.io/api/";
+  let data = {
+    model : "default",
+    input : `[${color1}, ${color2}, ${color3},"N","N"]`
+  };
+  
+  let request = new XMLHttpRequest();
+  
+  request.onreadystatechange = function() {
+    let palette = JSON.parse(request.responseText).result;
+    if(request.readyState == 4 && request.status == 200) {
+      printElements();
+      console.log(palette);
+    } else {
+      printError();
+      console.log(`${request.status}`);
+    }
+  };
+  request.open("POST", url, true);
+  request.send(JSON.stringify(data));
+}
 
 // UI Logic
 
+function printElements(apiResponse, city) {
+  document.querySelector('#showResponse').innerText = `The humidity in ${city} is ${apiResponse.main.humidity}%. 
+  The temperature in Kelvins is ${apiResponse.main.temp} degrees.`;
+}
 
+function printError(request, apiResponse, city) {
+  document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${city}: ${request.status} ${request.statusText}: ${apiResponse.message}`;
+}
 
+function getRgb(hex) {
+  const r = parseInt(hex.slice(1, 3), 16);
+  const g = parseInt(hex.slice(3, 5), 16);
+  const b = parseInt(hex.slice(5, 7), 16);
+  // return {r, g, b} // return an object
+  return [ r, g, b ];
+}
+
+function hideResults() {
+  document.getElementById("hat-selection").setAttribute("class", "hidden");
+  document.getElementById("inner-wear-selection").setAttribute("class", "hidden");
+  document.getElementById("outer-wear-selection").setAttribute("class", "hidden");
+  document.getElement ("shoes").setAttribute("class", "hidden");
+}
 
 function handleFormSubmission(event) {
   event.preventDefault();
-  // collecting the color selected by user
+  hideResults();
+  
   const keyColor = document.getElementById("color1").value;
   const accentColor1 = document.getElementById("color2").value;
   const accentColor2 = document.getElementById("color3").value;
-  // collect info from shoe and hat size boxes
-  const shoeSize = parseInt(document.querySelector('#shoes').value);
-  const hatSize = parseInt(document.querySelector('#hat-fit').value);
-  // collect clothing size
-  const clothingSize = document.getElementById("clothing-size").value;
-  // collect check box info
-  const userSelections = document.querySelectorAll("input[name=gear]:checked");
-  // we need to add an array
+
+  let color1 = getRgb(keyColor);
+  let color2 = getRgb(accentColor1);
+  let color3 = getRgb(accentColor2);
+  getPalette(color1, color2, color3);
+  console.log(color1);
+  console.log(color2);
+  console.log(color3);
+
+  // const shoeSize = parseInt(document.querySelector('#shoes').value);
+  // const hatSize = parseInt(document.querySelector('#hat-fit').value);
+  // const clothingSize = document.getElementById("clothing-size").value;
+  // const userSelections = document.querySelectorAll("input[name=gear]:checked");
+  // const userSelectionsArray = Array.from(userSelections);
+
+  // userSelectionsArray.forEach(function(element) {
+    // add if...else for displaying checkbox links
+
+  // });
 }
+
+
 
 window.addEventListener("load", function() {
   document.querySelector("form#clothing").addEventListener("submit", handleFormSubmission);

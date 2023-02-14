0a80f5ee0f998bcbbc72729b2d9ff7a50b179273
David Jandron <jandron1211@gmail.com>
2023-02-13 16:02:34 -0800

    ADD UI for form submission

--- src/index.js
+++ src/index.js
@@ -14,4 +14,29 @@ async function getThreads(city) {
   }
 }
 
-// <!-- const favoriteColor = document.getElementById("color").value; -->
+
+
+// UI Logic
+
+
+
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  // collecting the color selected by user
+  const keyColor = document.getElementById("color1").value;
+  const accentColor1 = document.getElementById("color2").value;
+  const accentColor2 = document.getElementById("color3").value;
+  // collect info from shoe and hat size boxes
+  const shoeSize = parseInt(document.querySelector('#shoes').value);
+  const hatSize = parseInt(document.querySelector('#hat-fit').value);
+  // collect clothing size
+  const clothingSize = document.getElementById("clothing-size").value;
+  // collect check box info
+  const userSelections = document.querySelectorAll("input[name=gear]:checked");
+  // we need to add an array
+}
+
+window.addEventListener("load", function() {
+  document.querySelector("form#clothing").addEventListener("submit", handleFormSubmission);
+});

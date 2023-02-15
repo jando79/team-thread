1abcce1a9b2db4196da836193c69aba9de6dec8e
Eliot Gronstal <eliot.lauren@gmail.com>
2023-02-15 09:27:19 -0800

    add two photos to images folder.
    Co-authored-by: Sarah Andyshak <sarah.andyshak@gmail.com>
    Co-authored-by: David Jandron  <jandron1211@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -5,52 +5,52 @@ const ESLintPlugin = require('eslint-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    devtool: 'eval-source-map',
-        devServer: {               
-          contentBase: './dist'   
-    },
-    plugins: [
-        new ESLintPlugin(),
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-          title: 'Shape Tracker',
-          template: './src/index.html',
-          inject: 'body'
-        }),
-        new Dotenv()
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.(gif|png|avif|jpe?g)$/,
-                use: [
-                  {
-                    loader: 'file-loader',
-                    options: {
-                      name: '[name].[ext]',
-                      outputPath: 'assets/images/'
-                    }
-                  }
-                ]
-                 },
-              {
-                test:/\.html$/,
-                use: [
-                  'html-loader'
-                ]
-            },
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'eval-source-map',
+  devServer: {               
+    contentBase: './dist'   
+  },
+  plugins: [
+    new ESLintPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Shape Tracker',
+      template: './src/index.html',
+      inject: 'body'
+    }),
+    new Dotenv()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
         ]
-    }
+      },
+      {
+        test: /\.(gif|png|avif|jpe?g)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/images/'
+            }
+          }
+        ]
+      },
+      {
+        test:/\.html$/,
+        use: [
+          'html-loader'
+        ]
+      },
+    ]
+  }
 };
